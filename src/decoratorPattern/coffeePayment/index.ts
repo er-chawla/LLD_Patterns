80d9fa8dc35a684fa3355f3494649449191ff587
er-chawla <er.manu27@gmail.com>
2025-09-22 22:49:26 +0530

    changes in coffee payment example

--- src/decoratorPattern/coffeePayment/index.ts
+++ src/decoratorPattern/coffeePayment/index.ts
@@ -5,5 +5,4 @@ import { SoyCondiments } from './decorators/soyCondiment';
 let espresso = new EspressoBeverage();
 espresso = new MochaCondiments(espresso);
 espresso = new MochaCondiments(espresso);
-espresso = new SoyCondiments(espresso);
 console.log(espresso.getDescription() + ' cost: $' + espresso.cost());
